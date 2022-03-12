baa775840eb38ecd5402c51800b8dd69ccff26d8
fotsingriemann <fotsingriemann@gmail.com>
2022-03-13 00:43:01 +0100

    initialisation des trois composants (header, leanding, footer)

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='footer-container'>
+                <p>Projet realiser par Bernoulli - 2022</p>
+                <p>Les icones de Wolverine, Iron-man, Spider-man, Batman sont sur iconFinder.com</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
