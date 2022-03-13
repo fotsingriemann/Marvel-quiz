84faa0d8a8ade685423fa85a1222b1eeea019629
fotsingriemann <fotsingriemann@gmail.com>
2022-03-13 02:18:48 +0100

    ajout des mouvement des griffe de wolverine aevc afficharge apres une minute

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,9 +1,38 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react'
 
 const Landing = () => {
-    return (
-        <main className='welcomePage'>
 
+
+    const refWolverine = React.useRef(null);
+
+    const [btn, setBtn] = React.useState(false)
+    
+    React.useEffect(() => {
+        refWolverine.current.classList.add("startingImg")
+
+        setTimeout(() => {
+            refWolverine.current.classList.remove("startingImg")
+            setBtn(true)
+        }, 1000)
+
+    }, [])
+
+
+    const displaybtn = btn && (
+    <React.Fragment>
+        <div className='leftBox'>
+            <button className='btn-welcome'> Inscription </button>
+        </div>
+        <div className='rightBox'>
+            <button className='btn-welcome'> Connexion </button>
+        </div>
+    </React.Fragment>
+    )
+
+    return (
+        <main ref={refWolverine} className='welcomePage'>
+            {displaybtn}
         </main>
     )
 }
